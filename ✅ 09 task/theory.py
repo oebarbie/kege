04bb328594f86968bb720c370cc14c0f90fec7f8
oebarbie <barbaraorekhova@gmail.com>
2025-06-08 15:28:16 +0300

    :)

--- ✅ 09 task/theory.py
+++ ✅ 09 task/theory.py
@@ -3,11 +3,5 @@
 # сохраняем на рабочий стол
 # 
 
-k = 0
-
 for s in open('09.txt'):
-    a = [int(x) for x in s.split()]
-    a = sorted(a)
-    if (a[-1]*a[0])**2 > 3*a[1]*a[2]*a[3]:
-        k += 1
-print(k)
+    a = sorted([int(x) for x in s.split()])
